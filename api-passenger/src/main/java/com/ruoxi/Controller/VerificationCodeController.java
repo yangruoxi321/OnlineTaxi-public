0e2a9c67a8de9fc020156ead07967c83694bb0ca
yangruoxi321 <yangruoxi321@gmail.com>
2024-01-26 21:47:51 -0500

    Update JSON handling and verification service
    The commit adds the JSON library to manage JSON data throughout the application. Additionally, significant changes have been made to the verification service. VerificationController is now renamed as VerificationCodeController and a verification code generation method has been added to the VerificationService. The workspace.xml, vcs.xml, pom.xml files have also been revised to support these changes.

--- api-passenger/src/main/java/com/ruoxi/Controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/ruoxi/Controller/VerificationCodeController.java
@@ -1,16 +1,20 @@
 package com.ruoxi.Controller;
 
 import com.ruoxi.Service.VerificationService;
+import com.ruoxi.requestEntity.getRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class VerificationController {
+public class VerificationCodeController {
     @Autowired
     private VerificationService verificationService;
     @GetMapping("/verification-code")
-    public String verify() {
-        return "Verification successful";
+   public String VerifyCode(@RequestBody getRequest request){
+        String phoneNumber = request.getPhoneNumber();
+        return verificationService.generateCode(phoneNumber);
+
     }
 }
--- api-passenger/src/main/java/com/ruoxi/Service/VerificationService.java
+++ api-passenger/src/main/java/com/ruoxi/Service/VerificationService.java
@@ -1,5 +1,24 @@
 package com.ruoxi.Service;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class VerificationService {
+    public String generateCode(String phoneNumber){
+        String code = "123456";
+        //TODO:Save code to redis with ttl
+        //return result{
+        //    "code":200,
+        //    "message":"success"
+        //}
+        JSONObject returnJson = new JSONObject();
+        returnJson.put("code",200);
+        returnJson.put("message","success");
+        return returnJson.toString();
+    }
 
 }
